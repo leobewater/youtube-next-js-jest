575ec28454465ba68663d23727cb81e868a0a544
leobewater <mmbee888@gmail.com>
2023-09-01 11:30:22 -0400

    add server setting before and after running tests

--- jest.setup.js
+++ jest.setup.js
@@ -5,4 +5,23 @@
 // Learn more: https://github.com/testing-library/jest-dom
 
 //import '@testing-library/jest-dom/extend-expect' // use this when @testing-library/jest-dom is before v6.0.0
-import '@testing-library/jest-dom' // use this when @testing-library/jest-dom is v6.0
+import '@testing-library/jest-dom'; // use this when @testing-library/jest-dom is v6.0
+import { server } from './mocks/server'
+;
+
+// before running all tests
+beforeAll(() => {
+  // Establish requests interception layer before all tests.
+  server.listen();
+});
+
+// reset each handlers after each test
+afterEach(() => {
+    server.resetHandlers();
+})
+
+afterAll(() => {
+  // Clean up after all tests are done, preventing this
+  // interception layer from affecting irrelevant tests.
+  server.close();
+});
