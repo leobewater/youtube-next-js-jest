28c7edf8440acd5b667fd756318e755ed3aaf9c7
leobewater <mmbee888@gmail.com>
2023-09-01 09:03:49 -0400

    add css file

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
-
+import styles from './page.module.css'
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <h1>Homepage</h1>
     </main>
   );
