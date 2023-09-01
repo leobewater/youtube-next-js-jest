875dd16039b166daa4e3b2d390237d569d90edff
leobewater <mmbee888@gmail.com>
2023-09-01 09:47:31 -0400

    test button text

--- __tests__/HomePage.test.tsx
+++ __tests__/HomePage.test.tsx
@@ -1,8 +1,16 @@
 import Home from '@/app/page';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 describe('Home Page - Rendering', () => {
   it('should have Home Page text', () => {
     render(<Home />);
-    screen.getByText('Homepage');
+    expect(screen.getByText('Homepage')).toBeInTheDocument();
   });
+
+  it('should have button with text Click Me', () => {
+    render(<Home />);
+    // search for button with text Click Me
+    expect(screen.getByRole("button", {name: 'Click Me'})).toBeInTheDocument();
+  })
 });
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Homepage</h1>
+      <button>Click Me</button>
+      <input />
     </main>
   );
 }
