07c9c09267548862c2c8cdabaa07177350eac15d
leobewater <mmbee888@gmail.com>
2023-09-01 12:46:50 -0400

    fix mock json return as array

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -4,11 +4,6 @@ import { rest } from 'msw';
 export const handlers = [
   // mock endpoint and return json object
   rest.get('/api/users', (req, res, ctx) => {
-    return res(
-      ctx.json({
-        id: 1,
-        username: 'anson',
-      })
-    );
+    return res(ctx.json([{ id: 1, username: 'anson' }]));
   }),
 ];
